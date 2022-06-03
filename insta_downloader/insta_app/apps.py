dd70d5909d24160ea5f2bb20f6dbc6bf781b5bf2
Kanha Tyagi <69853926+tyagi-space@users.noreply.github.com>
2022-06-03 15:25:07 +0530

    Delete insta_downloader directory

--- insta_downloader/insta_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InstaAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'insta_app'
--- insta_downloader/insta_app/form.py
+++
@@ -1,7 +0,0 @@
-from . import models
-from django import forms
-
-class URLForm(forms.ModelForm):
-    class Meta():
-        model = models.URLinput
-        fields = ('URLvalue',)
